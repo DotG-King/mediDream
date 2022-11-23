63251d4309611ef4516ddf069bf455bd3a873b91
wwwonwo <wwwonwo@gmail.com>
2022-11-23 14:03:04 +0900

    upload

---
+++ mediDream/search/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from list.models import search
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = search
+        fields = {'img'}
--- mediDream/search/views.py
+++ mediDream/search/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import UploadForm
 
 # Create your views here.
 
-def search_view(request) :
-    return HttpResponse('검색 페이지 입니다.')
+# def search_view(request) :
+#     return HttpResponse('검색 페이지 입니다.')
+
+def index(request):
+    return render(request,'search/index.html', {})
+
+def image_list(request):
+    return render(request,'search/list.html', {})
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('image_list')
+    else:
+        form = UploadForm()
+    return render(request,'search/upload.html',{
+        'form': form
+    })
