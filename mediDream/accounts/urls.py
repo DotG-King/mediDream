2824d075e875f5c16b0de73e494d8986253800eb
DotG-King <tms02171@gmail.com>
2022-12-01 13:53:24 +0900

    update

--- mediDream/accounts/urls.py
+++ mediDream/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('',views.index,name='main'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
