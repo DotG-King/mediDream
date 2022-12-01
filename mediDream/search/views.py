4e338e9293447dfbe2853ae20b906285a3b774d2
wwwonwo <wwwonwo@gmail.com>
2022-12-01 11:42:23 +0900

    search&list connect

--- mediDream/search/views.py
+++ mediDream/search/views.py
@@ -22,12 +22,15 @@ def upload_image(request):
         form = UploadForm(request.POST, request.FILES)
 
         if form.is_valid():
-            tmp=search()
-            tmp.mediDream_id=User.objects.get(username=request.user.username)
-            tmp.img=request.FILES['img']
-            tmp.pill=pillInfo.objects.get(id=img_clf(tmp.img))
-            tmp.save()
-            return redirect('image_list')
+            predict=search()
+            predict.mediDream_id=User.objects.get(username=request.user.username)
+            predict.img=request.FILES['img']
+            predict_result=img_clf(predict.img)
+            predict.pill=pillInfo.objects.get(id=predict_result)
+            predict.save()
+            test=pillInfo.objects.all().filter(id=predict_result)
+            return render(request, 'list/info.html', {'test':test})
+
     else:
         form = UploadForm()
     return render(request,'search/upload.html',{ 'form': form })
