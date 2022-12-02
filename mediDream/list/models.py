7f730fffacc9b3968e948428aa36abe5976cbd1e
wwwonwo <wwwonwo@gmail.com>
2022-12-02 11:10:56 +0900

    upload_size_limit&path_change

--- mediDream/list/models.py
+++ mediDream/list/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import MEMBERS
 from django.utils import timezone
+from search.validators import validate_file
+
+import os
+from uuid import uuid4
+from django.utils import timezone
+
+def date_upload_to(instance, filename):
+  # upload_to="%Y/%m/%d" 처럼 날짜로 세분화
+  ymd_path = timezone.now().strftime('%Y/%m/%d') 
+  # 길이 32 인 uuid 값
+  uuid_name = uuid4().hex
+  # 확장자 추출
+  extension = os.path.splitext(filename)[-1].lower()
+  # 결합 후 return
+  return '/'.join([
+    ymd_path,
+    uuid_name + extension,
+  ])
 
 # Create your models here.
 
@@ -22,4 +40,4 @@ class search(models.Model):
     mediDream_id=models.ForeignKey(User,on_delete=models.PROTECT,null=True)  # 유저 아이디
     pill=models.ForeignKey(pillInfo,on_delete=models.PROTECT,null=True)         # 약
     date=models.DateTimeField(default=timezone.now)                             # 시간, default 설정
-    img= models.ImageField(null=True, blank=True, upload_to='')
+    img= models.ImageField(null=True, blank=True, upload_to=date_upload_to,validators=[validate_file])
