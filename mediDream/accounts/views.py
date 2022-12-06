66b428724bf48d6ca4c3042c8c8090bdff9da213
zisu <seo101500@gmail.com>
2022-12-06 13:36:12 +0900

    nav

--- mediDream/accounts/views.py
+++ mediDream/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 LOGIN_TRY_LIMIT=5
 count=0
